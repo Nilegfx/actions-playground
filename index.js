375392d5af6a3f7d47b0628d449ae0b58a5f6a5e
Ahmed Ayoub <great.nile@gmail.com>
2020-09-10 20:49:44 +0200

    check async

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+const wait = (time) => new Promise((res) => {
+setTimeout(res, time)
+})
+
+const run = async () => {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput("who-to-greet");
+    console.log(`Hello ${nameToGreet}!`);
+    const time = new Date().toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+
+    await wait(60000);
+    console.log('finished after async')
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+run();
